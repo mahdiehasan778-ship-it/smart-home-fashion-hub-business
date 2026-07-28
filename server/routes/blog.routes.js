699b0b8b1c0aed39f8d71d917c13827e563458fb
Mahdie Hasan <mahdiehasan778@gmail.com>
2026-07-28 15:06:05 +0600

    Move blog.routes.js to routes folder

--- server/routes/blog.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getBlogPosts } from '../controllers/blog.controller.js';
-
-const router = Router();
-
-router.get('/', getBlogPosts);
-
-export default router;
