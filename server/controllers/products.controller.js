29a88e904ac4a7942d268cbe754d968fe01505b1
Mahdie Hasan <mahdiehasan778@gmail.com>
2026-07-28 15:38:09 +0600

    Fix getProductBySlug export name

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -3,9 +3,10 @@ import { Router } from 'express';
 const router = Router();
 
 export const getProducts = (req, res) => { res.json([]); };
+export const getProductBySlug = (req, res) => { res.json({}); };
 export const getProductById = (req, res) => { res.json({}); };
 
 router.get('/', getProducts);
-router.get('/:id', getProductById);
+router.get('/:slug', getProductBySlug);
 
 export default router;
